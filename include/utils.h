3ad99cf09f91b405deb50a3d8466cb9d6e24f5ec
Clement GUICHARD <clement.guichard1@etu.univ-orleans.fr>
2018-09-23 22:28:08 +0200

    utils.h and utils.c documented

--- include/utils.h
+++ include/utils.h
@@ -1,9 +1,30 @@
-#ifndef UTILS_H
-#define UTILS_H
+/**
+ * @file: utils.h
+ * @author: GUICHARD Clément
+ * @standard: C99
+ *
+ * @brief: Header file for "utils.c"
+ *
+ * This file contains tool functions, used by
+ * other files but not really belonging to a file,
+ * and being useful in many places of the project
+ *
+ * Overview about public functions of utils:
+ *  - equalsInt
+ *  - printInt
+ *  - printChar
+ *  - copyInt
+ *  - copyChar
+ *  - pointerAllocError
+ *  - pointerNullError
+ */
 
 /* ========================================================= */
-/* ================== TUPLE_H FILE HEADER ================== */
-/* ========================================================= */
+/* ================== UTILS_H FILE HEADER ================== */
+/* ========================================================================== */
+
+#ifndef UTILS_H
+#define UTILS_H
 
 /* ========= Includes ======== */
 
@@ -12,14 +33,97 @@
 
 /* ======== Functions ======== */
 
+/** ===========================================================================/
+* @function: equalsInt
+* @brief: Tell if two integer are equals
+*
+* This function tell if two given integer values are equals. The function used
+* generic pointers to make the function generic, so this is the pointers of the
+* integers that's needed to be pass
+*
+* @param{void*} val1: pointer on the first integer (the pointer is generic)
+* @param{void*} val2: pointer on the second integer (the pointer is generic)
+*
+* @return{int}: 0 for false and other values equals to true
+*/
 int equalsInt(void *val1, void *val2);
+
+/** ===========================================================================/
+* @function: printInt
+* @brief: Print an integer
+*
+* This function print an integer. The generic pointer of the integer is given
+* to the function
+*
+* @param{void*} elem: pointer on the integer (the pointer is generic)
+*
+* @return{void}
+*/
 void printInt(void *elem);
+
+/** ===========================================================================/
+* @function: printChar
+* @brief: Print a character
+*
+* This function print a character. The generic pointer of the character is given
+* to the function
+*
+* @param{void*} elem: pointer on the character (the pointer is generic)
+*
+* @return{void}
+*/
 void printChar(void *elem);
+
+/** ===========================================================================/
+* @function: copyInt
+* @brief: Return the pointer of the copy of the given integer
+*
+* This function allocates a new integer in the memory, copies the value of
+* the given integer into the new, and returns the pointer of the copy
+*
+* @param{void*} elem: pointer on the integer to copy (the pointer is generic)
+*
+* @return{void*}: The generic pointer of the new integer
+*/
 void* copyInt(void *elem);
+
+/** ===========================================================================/
+* @function: copyChar
+* @brief: Return the pointer of the copy of the given character
+*
+* This function allocates a new character in the memory, copies the value of
+* the given character into the new, and returns the pointer of the copy
+*
+* @param{void*} elem: pointer on the character to copy (the pointer is generic)
+*
+* @return{void*}: The generic pointer of the new character
+*/
 void* copyChar(void *elem);
+
+/** ===========================================================================/
+* @function: pointerAllocError
+* @brief: Print a memory allocation error, and stop the program
+*
+* This function is called when an allocation has failed, it prints an error and
+* stops the program
+*
+* @return{void}
+*/
 void pointerAllocError();
+
+/** ===========================================================================/
+* @function: pointerNullError
+* @brief: Print a null pointer error, and stop the program
+*
+* This function is called when a function pointer that you need not null is
+* null, it prints an error and stops the program
+*
+* @return{void}
+*/
 void pointerNullError();
 
-/* ========================================================= */
 
 #endif
+
+/* ========================================================================== */
+/* ========================================================================== */
--- src/utils.c
+++ src/utils.c
@@ -1,39 +1,87 @@
+/**
+ * @file: utils.c
+ * @author: GUICHARD Clément
+ * @standard: C99
+ *
+ * @brief: Implementation file for "utils.h"
+ *
+ * This file contains tool functions, used by
+ * other files but not really belonging to a file,
+ * and being useful in many places of the project
+ *
+ * Overview about public functions of utils:
+ *  - equalsInt
+ *  - printInt
+ *  - printChar
+ *  - copyInt
+ *  - copyChar
+ *  - pointerAllocError
+ *  - pointerNullError
+ */
+
 #include "utils.h"
+
 
 /* ================================================== */
 /* ===================== PUBLIC ===================== */
-/* ============================================================================ */
+/* ========================================================================== */
 
+
+/** ===========================================================================/
+ * @see: @file utils.h / @function equalsInt
+ */
 int equalsInt(void *val1, void *val2){
   return *((int*)val1) == *((int*)val2);
 }
 
+/** ===========================================================================/
+ * @see: @file utils.h / @function printInt
+ */
 void printInt(void *elem){
   printf("%d", *((int*)elem));
 }
 
+/** ===========================================================================/
+ * @see: @file utils.h / @function printChar
+ */
 void printChar(void *elem){
   printf("'%c'", *((char*)elem));
 }
 
+/** ===========================================================================/
+ * @see: @file utils.h / @function copyInt
+ */
 void* copyInt(void *elem){
   int* i = (int*)malloc(sizeof(int));
   *i = *((int*)elem);
   return i;
 }
 
+/** ===========================================================================/
+ * @see: @file utils.h / @function copyChar
+ */
 void* copyChar(void *elem){
   char* i = (char*)malloc(sizeof(char));
   *i = *((char*)elem);
   return i;
 }
 
+/** ===========================================================================/
+ * @see: @file utils.h / @function pointerAllocError
+ */
 void pointerAllocError(){
   printf("Memory error: memory allocation can't be done\n");
-  exit(1);
+  exit(0);
 }
 
+/** ===========================================================================/
+ * @see: @file utils.h / @function pointerNullError
+ */
 void pointerNullError(){
   printf("Null pointer error: a function pointer needed to not be null has been found null\n");
-  exit(1);
+  exit(0);
 }
+
+
+/* ========================================================================== */
+/* ========================================================================== */
